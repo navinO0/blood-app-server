01775cc6507d707e948b7ac57b768fa1cd07f343
navinO0 <6y.navin@gmail.com>
2025-12-07 15:26:33 +0530

    fix-socket spamming

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      process.env.FRONTEND_URL,
+      process.env.FRONTEND_URL_SOCKET
+    ].filter(Boolean),
     methods: ["GET", "POST"],
   },
 });
